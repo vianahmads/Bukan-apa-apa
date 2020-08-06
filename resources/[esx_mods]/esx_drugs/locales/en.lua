c150ddb42221606086d130d31c1dfe154199872d
Vian Saputra <vianahmads97@gmail.com>
2020-08-06 09:48:23 +0700

    Added Money Wash

---
+++ resources/[esx_mods]/esx_drugs/locales/en.lua
@@ -0,0 +1,114 @@
+Locales ['en'] = {
+  -- weed
+  ['weed_pickupprompt'] = 'tekan ~INPUT_CONTEXT~ untuk memetik tanaman ~g~Sabu~s~.',
+  ['weed_inventoryfull'] = 'kantongmu tidak cukup untuk tanaman ~g~Sabu~s~.',
+  ['weed_processprompt'] = 'takan ~INPUT_CONTEXT~ untuk memulai ~g~Proses Sabu~s~.',
+  ['weed_processingstarted'] = 'memproses ~g~Sabu~s~ menjadi ~g~Marijuana~s~...',
+  ['weed_processingfull'] = 'proses ~r~dibatalkan~s~ karena kantong penuh!',
+  ['weed_processingenough'] = 'kamu harus memiliki ~b2x~s~ ~g~Sabu~s~ untuk memulai proses.',
+  ['weed_processed'] = 'kamu telah memproses ~b~2x~s~ ~g~Sabu~s~ menjadi ~b~1x~s~ ~g~Marijuana~s~',
+  ['weed_processingtoofar'] = 'proses ~r~Dibatalkan~s~ karena kamu meninggalkan area.',
+  
+  -- meth
+  ['HydrochloricAcid_pickupprompt'] = 'press ~INPUT_CONTEXT~ to pickup the ~g~HydroChloric Acid~s~ barrel.',
+  ['SulfuricAcid_pickupprompt'] = 'press ~INPUT_CONTEXT~ to pickup the ~g~Sulfuric Acid~s~ barrel.',
+  ['SodiumHydroxide_pickupprompt'] = 'press ~INPUT_CONTEXT~ to pickup the ~g~Sodium Hydroxide~s~ barrel.',
+  ['HydrochloricAcid_inventoryfull'] = 'you do not have any more inventory space for ~g~HydroChloric Acid~s~.',
+  ['SulfuricAcid_inventoryfull'] = 'you do not have any more inventory space for ~g~Sulfuric Acid~s~.',
+  ['sodium_hydroxide_inventoryfull'] = 'you do not have any more inventory space for ~g~Sodium Hydroxide~s~.',
+  ['meth_processprompt'] = 'press ~INPUT_CONTEXT~ to start to ~g~Process Meth~s~.',
+  ['meth_processingstarted'] = 'processing ~g~Meth~s~...',
+  ['meth_processingfull'] = 'processing ~r~canceled~s~ due to full inventory!',
+  ['meth_processingenough'] = 'you must have ~b~1x~s~ of everything in order to process.',
+  ['meth_processed'] = 'you\'ve processed ~b~1x~s~ ~g~meth~s~',
+  ['meth_processingtoofar'] = 'the processing has been ~r~canceled~s~ due to you abandoning the area.',
+  
+  -- coke
+  ['coke_pickupprompt'] = 'Tekan ~INPUT_CONTEXT~ untuk memetik daun ~g~Ganja~s~.',
+  ['coke_inventoryfull'] = 'kantongmu tidak cukup untuk ~g~daun Ganja~s~.',
+  ['coke_processprompt'] = 'Tekan ~INPUT_CONTEXT~ untuk memulai ~g~Proses Ganja~s~.',
+  ['coke_processingstarted'] = 'memproses ~g~Ganja~s~...',
+  ['coke_processingfull'] = 'proses ~r~dibatalkan~s~ karena kantong penuh!',
+  ['coke_processingenough'] = 'Kamu harus memiliki ~b~3x~s~ ~g~daun Ganja~s~ untuk memulai proses.',
+  ['coke_processed'] = 'Kamu telah memproses ~b~1x~s~ ~g~Ganja~s~',
+  ['coke_processingtoofar'] = 'proses ~r~Dibatalkan~s~ karena kamu meninggalkan area.',
+  
+  -- lsd
+  ['lsd_processingstarted'] = 'processing ~g~LSD~s~...',
+  ['lsd_processingfull'] = 'processing ~r~canceled~s~ due to full inventory!',
+  ['lsd_processprompt'] = 'press ~INPUT_CONTEXT~ to start to ~g~Process LSD~s~.',
+  ['lsd_processingenough'] = 'you must have ~b~1x~s~ ~g~LSA~s~ and ~b~1x~s~ ~g~Thionyl Chloride~s~ in order to process.',
+  ['lsd_processed'] = 'you\'ve processed ~b~1x~s~ ~g~LSD~s~',
+  ['lsd_processingtoofar'] = 'the processing has been ~r~canceled~s~ due to you abandoning the area.',
+  
+  ['thionylchloride_processingstarted'] = 'processing ~g~Thionyl Chloride~s~...',
+  ['thionylchloride_processingfull'] = 'processing ~r~canceled~s~ due to full inventory!',
+  ['thionylchloride_processprompt'] = 'press ~INPUT_CONTEXT~ to start to ~g~Process Thionyl Chloride~s~.',
+  ['thionylchloride_processingenough'] = 'you must have ~b~1x~s~ ~g~LSA~s~ and ~b~1x~s~ ~g~Chemicals~s~ in order to process.',
+  ['thionylchloride_processed'] = 'you\'ve processed ~b~1x~s~ ~g~Thionyl Chloride~s~',
+  ['thionylchloride_processingtoofar'] = 'the processing has been ~r~canceled~s~ due to you abandoning the area.',
+  
+   -- heroin
+  ['heroin_pickupprompt'] = 'press ~INPUT_CONTEXT~ to harvest the ~g~Poppy~s~ plant.',
+  ['poppy_inventoryfull'] = 'you do not have any more inventory space for ~g~Poppy Resin~s~.',
+  ['heroin_inventoryfull'] = 'you do not have any more inventory space for ~g~Heroin~s~.',
+  ['heroin_processprompt'] = 'press ~INPUT_CONTEXT~ to start ~g~Process Heroin~s~.',
+  ['heroin_processingstarted'] = 'processing ~g~Poppy Resin~s~ into ~g~Heroin~s~...',
+  ['heroin_processingfull'] = 'processing ~r~canceled~s~ due to full inventory!',
+  ['heroin_processingenough'] = 'you must have ~b~1x~s~ ~g~Poppy Resin~s~ in order to process.',
+  ['heroin_processed'] = 'you\'ve processed ~b~1x~s~ ~g~Heroin~s~',
+  ['heroin_processingtoofar'] = 'the processing has been ~r~canceled~s~ due to you abandoning the area.',
+  
+  -- chemicals
+  ['chemicals_pickupprompt'] = 'press ~INPUT_CONTEXT~ to retrieve the ~g~Chemicals~s~',
+  ['chemicals_inventoryfull'] = 'you do not have any more inventory space for ~g~Chemicals~s~.',
+  ['Chemicals_notenough'] = 'you do not have any more ~g~Chemicals~s~ to process.',
+  ['Chemicals_made'] = 'you\'ve made ~b~1x~s~ ~y~%s~s~',
+  ['chemicals_prompt'] = 'press ~INPUT_CONTEXT~ to open the ~r~Chemical Conversion Menu~s~.',
+  ['ChemicalsConvertion_title'] = 'Chemical Conversion Menu',
+  ['chemicals_making'] = '~g~Making...~s~',
+  
+  -- drug dealer
+  ['dealer_prompt'] = 'press ~INPUT_CONTEXT~ to talk with the ~r~Drug Dealer~s~.',
+  ['dealer_title'] = 'drug Dealer',
+  ['dealer_item'] = '$%s',
+  ['dealer_notenough'] = 'you don\'t have enough of that to sell!',
+  ['dealer_sold'] = 'you\'ve sold ~b~%sx~s~ ~y~%s~s~ for ~g~$%s~s~',
+
+  -- license
+  ['license_title'] = 'you are required to own a processing license to process this product, it can be bought here.',
+  ['license_no'] = 'no',
+  ['license_bought'] = 'you bought the ~b~%s~s~ for ~r~$%s~s~',
+  ['license_bought_fail'] = 'you cannot afford an ~b~%s~s~!',
+  ['license_weed'] = 'weed Processing License',
+  ['license_meth'] = 'meth Processing License',
+  ['need_license'] = 'You need a License to use this.',
+  ['licenseshop_title'] = 'License Shop',
+  ['licenseshop_prompt'] = 'press ~INPUT_CONTEXT~ to open with the ~g~License Shop~s~.',
+  ['license_bought'] = 'you\'ve bought ~b~1x~s~ ~y~%s~s~ for ~g~$%s~s~',
+  ['license_inventoryfull'] = 'you allready have this License on you.',
+  
+  -- moneywash
+  ['moneywash_washed'] = 'you\'ve washed ~r~$%s~s~',
+  ['moneywash_title'] = 'MoneyWash',
+  ['moneywash_wash'] = 'Wash all black money.',
+  ['moneywash_prompt'] = 'press ~INPUT_CONTEXT~ to open the ~r~MoneyWash~s~.',
+  
+  -- blips
+  ['blip_weedfield'] = 'weed Field',
+  ['blip_weedprocessing'] = 'weed Processing',
+  ['blip_methfield'] = 'meth Field',
+  ['blip_methprocessing'] = 'meth Processing',
+  ['blip_drugdealer'] = 'drug Dealer',
+  ['blip_ChemicalsProcessing'] = 'Chemische Verarbeitung',
+  ['blip_ChemicalsFarm'] = 'Chemiefarm',
+  ['blip_CokeFarm'] = 'Koks Farm',
+  ['blip_Cokeprocessing'] = 'Koks Herstellung',
+  ['blip_herionfield'] = 'Heroin Farm',
+  ['blip_heroinprocessing'] = 'Heroin Herstellung',
+  ['blip_HydrochloricAcidFarm'] = 'Salzsäure Bauernhof',
+  ['blip_lsdprocessing'] = 'LSD Herstellung',
+  ['blip_SodiumHydroxideFarm'] = 'Sodium Hydroxide Farm',
+  ['blip_SulfuricAcidFarm'] = 'SulfuricAcidFarm',
+}
+
